83e3517235e81d4fa6e6541a8ced8bc8e535ad96
jester <jaimebuddy98@gmail.com>
2022-02-12 16:35:49 -0600

    added a post model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes, DATE } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING, 
+            allowNull: false
+        },
+        pot_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
