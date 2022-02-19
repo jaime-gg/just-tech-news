c751465363279ce71cd6cf1a55dbcf368dd7f842
jester <jaimebuddy98@gmail.com>
2022-02-19 12:50:22 -0600

    worked on login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true }));
+
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+
   Post.findAll({
     attributes: [
       'id',
@@ -28,6 +30,7 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
       // pass a single post object into the homepage template
       res.render('homepage', { posts });
     })
@@ -35,6 +38,15 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
